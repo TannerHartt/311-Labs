6526c598fc56e35199d270b78fb22287f598ba93
Tanner Hart <tannerhart24@yahoo.com>
2024-04-12 22:13:44 -0700

    organized into folder

--- project2/src/AirportDrive.cpp
+++ project2/src/AirportDrive.cpp
@@ -100,6 +100,15 @@ class Plane {
 ************************************************/
 //SECTION_B_START: put all your classes definitions here.
 
+/* Methods to implement
+- empty() - returns true if the priority queue is empty and false otherwise.
+• size() - returns the number of elements in the priority queue.
+• push(Object o) - a void method which adds the object o to the priority queue. (Object can be
+replaced with your own class.)
+• pop() - removes and returns the first element of the priority queue.
+• peek() - returns, but does not remove, the first element of the priority queue.
+*/
+
 
 /// @brief Priority Queue class that works specifically with Plane objects implemented using a max heap
 class PQueue {
--- project2/src/Queue.cpp
+++ project2/src/Queue.cpp
@@ -3,14 +3,7 @@
 #include "Plane.h"
 using namespace std;
 
-/* Methods to implement
-- empty() - returns true if the priority queue is empty and false otherwise.
-• size() - returns the number of elements in the priority queue.
-• push(Object o) - a void method which adds the object o to the priority queue. (Object can be
-replaced with your own class.)
-• pop() - removes and returns the first element of the priority queue.
-• peek() - returns, but does not remove, the first element of the priority queue.
-*/
+
 
 class Queue {
   /*
